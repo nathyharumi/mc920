e2fc9c3df7ed5fb9263c7e84d81c5f6c85206e79
nathyharumi <nathalia.kuromiya@gmail.com>
2019-05-06 03:31:45 -0300

    fix the ordered dithering

--- t2/dithering.py
+++ t2/dithering.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import numpy as np
 
 def _get_image(filename):
@@ -7,11 +8,13 @@ def _get_image(filename):
 def _save_image(image, filename, sufix):
     if not os.path.exists("results"):
         os.mkdir("results")
-    saving_filename = "results/" + filename.split("/")[-1][:-4] + "_"
-    cv2.imwrite(saving_filename + sufix, image)
+    saving_filename = "results/" + filename.split("/")[-1][:-4] + "_" + sufix
+    with open(saving_filename, 'w') as fd:
+        fd.write("P4\n{} {}\n".format(image.shape[0], image.shape[1]))
+        np.packbits(image, axis=-1).tofile(fd)
 
-def _normalize(image, min, max):
-    return (image - image.min())*(max - min)/(image.max() - image.min()) + min
+def _transform(image, max):
+    return np.multiply(image, max/255)
 
 def ordered_dithering(filename):
     image = _get_image(filename)
@@ -23,20 +26,41 @@ def ordered_dithering(filename):
                        "[1, 0, 3]\n"
                        "[5, 2, 7]\n"
                        "2 - Bayer Kernel\n")) == 1:
-        result = dither(image, simple_kernel)
-        _save_image(result, filename, "simple_kernel.bpm")
+        result = _dither(image, simple_kernel)
+        _save_image(result, filename, "simple_kernel.pbm")
     else:
-        result = dither(image, bayer_kernel)
-        _save_image(result, filename, "bayer_kernel.bpm")
+        result = _dither(image, bayer_kernel)
+        _save_image(result, filename, "bayer_kernel.pbm")
 
-def dither(image, kernel):
-    result = np.zeros(image.shape)
+def _dither(image, kernel):
+    result = np.zeros(image.shape, dtype=np.int16)
     (x, y) = image.shape
-    normalized_image = _normalize(image, 0, kernel.shape[0]*kernel.shape[1] + 1)
+    transformed_image = _transform(image, kernel.shape[0]*kernel.shape[1] + 1)
+
     for i in range(x):
         for j in range (y):
-            if normalized_image[x][y] < kernel[x % kernel.shape[0]][y % kernel.shape[1]]:
-                result[x][y] = 0
+            if transformed_image[i][j] < kernel[i % kernel.shape[0]][j % kernel.shape[1]]:
+                result[i][j] = 1
             else:
-                result[x][y] = 255
+                result[i][j] = 0
     return result
+
+def fs_dithering(filename):
+    image = _get_image(filename)
+    result = image//129
+    error = image - result
+
+
+def main():
+    dithering_method = int(input("Enter a dithering method [1-2]:\n"
+                                 "1 - Ordered dithering\n"
+                                 "2 - Floyd-Steinberg\n"))
+    filename = input("Enter the filepath of the image:\n")
+
+    if dithering_method == 1:
+        ordered_dithering(filename)
+    else:
+        fs_dithering(filename)
+
+if __name__ == '__main__':
+    main()
