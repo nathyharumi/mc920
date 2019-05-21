0786659df47b15966bf6f219b018dd110d5d3929
nathyharumi <nathalia.kuromiya@gmail.com>
2019-05-21 00:52:51 -0300

    finish text_components.py

--- t3/text_components.py
+++ t3/text_components.py
@@ -50,34 +50,61 @@ def closing(image, kernel):
     """
     return cv2.erode(cv2.dilate(image, kernel), kernel)
 
-def row_column_closing(image, row_kernel, column_kernel):
-    image = cv2.bitwise_not(image)
-
-    row_closing = closing(image, row_kernel)
-    column_closing = closing(image, column_kernel)
-    return row_closing, column_closing
-
 def preprocessing(image):
+    """ Preprocess the image to make each line as a connected component
+
+    Args:
+    image (np.ndarray): original image
+
+    Returns:
+    np.ndarray: preprocessed image
+    """
     ROW_STRUCTURAL_KERNEL = np.ones((1, 100))
     COLUMN_STRUCTURAL_KERNEL = np.ones((200, 1))
     POS_STRUCTURAL_KERNEL = np.ones((1, 30))
 
-    row_closing, column_closing = row_column_closing(image,
-        ROW_STRUCTURAL_KERNEL, COLUMN_STRUCTURAL_KERNEL)
+    image = cv2.bitwise_not(image)
+
+    row_closing = closing(image, ROW_STRUCTURAL_KERNEL)
+    column_closing = closing(image, COLUMN_STRUCTURAL_KERNEL)
+
     merged_image = cv2.bitwise_and(row_closing, column_closing)
 
     return closing(merged_image, POS_STRUCTURAL_KERNEL)
 
 def find_words_preprocessing(image):
-    ROW_STRUCTURAL_KERNEL = np.ones((1, 10))
+    """ Preprocess the image to find words components
+
+    Args:
+    image (np.ndarray): original image
+
+    Returns:
+    np.ndarray: preprocessed image
+    """
+    ROW_STRUCTURAL_KERNEL = np.ones((1, 12))
     COLUMN_STRUCTURAL_KERNEL = np.ones((10, 1))
 
-    row_closing, column_closing = row_column_closing(image,
-        ROW_STRUCTURAL_KERNEL, COLUMN_STRUCTURAL_KERNEL)
+    image = cv2.bitwise_not(image)
+
+    row_closing = closing(image, ROW_STRUCTURAL_KERNEL)
+    column_closing = closing(image, COLUMN_STRUCTURAL_KERNEL)
 
     return cv2.bitwise_or(row_closing, column_closing)
 
 def get_ratios_per_component(image, component):
+    """ Get the black pixels ratio (number of black pixels/the area of the
+    component) and the transitions ratio (number of transitions black to white
+    pixel/number of black pixels)
+
+    Args:
+    image (np.ndarray):
+    component (list): list containing the x_min, y_min, the width, the high and
+    the area of the component
+
+    Returns:
+    float: ratio of black pixels
+    float: ratio of black-to-white transitions
+    """
     area = component[2] * component[3]
     black_pixels = _get_black_pixels_per_component(image, component)
     transitions = _get_transitions_per_component(image, component)
@@ -87,6 +114,16 @@ def get_ratios_per_component(image, component):
     return black_pixels_ratio, transitions_ratio
 
 def _get_black_pixels_per_component(image, component):
+    """ Count the number of black pixels in a component area of the image
+
+    Args:
+    image (np.ndarray): original image
+    component (list): list containing the x_min, y_min, the width, the height
+    and the area of the component
+
+    Returns:
+    int: number of black pixels
+    """
     x_min, y_min, w, h, area = component
     black_pixels = 0
     for i in range(h):
@@ -98,6 +135,17 @@ def _get_black_pixels_per_component(image, component):
     return black_pixels
 
 def _get_transitions_per_component(image, component):
+    """ Count the number of black-to-white transitions in a component area of
+    the image
+
+    Args:
+    image (np.ndarray): original image
+    component (list): list containing the x_min, y_min, the width, the height
+    and the area of the component
+
+    Returns:
+    int: number of transitions
+    """
     x_min, y_min, w, h, area = component
     transitions = 0
     for i in range(h - 1):
@@ -111,15 +159,34 @@ def _get_transitions_per_component(image, component):
     return transitions
 
 def get_components_coordenates(image):
+    """ Get all components coordenates of a image
+
+    Args:
+    image (np.ndarray): preprocessed image where to find the components
+
+    Returns:
+    list: list of the stats list (x_min, y_min, width, height and area) of all
+    components found.
+    """
     n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(image)
     return stats
 
 def get_text_components(original_image, components):
+    """ Filter the text components based on ratio rules.
+
+    Args:
+    original_image(np.ndarray): the original image
+    components(list): all components
+
+    Returns:
+    list: text components
+
+    """
     # Rules
-    MIN_BP_RATIO = 0.2
-    MAX_BP_RATIO = 0.9
-    MIN_TR_RATIO = 0.0
-    MAX_TR_RATIO = 1.0
+    MIN_BP_RATIO = 0.5 #0.5 pra 0.6 cortou os números
+    MAX_BP_RATIO = 0.8 #0.8 pra 0.7 cortou muita linha
+    MIN_TR_RATIO = 0.1 #0.1 pra 0.2 cortou muita linha
+    MAX_TR_RATIO = 0.6 #0.6 pra 0.5 cortou os números
 
     text_components = []
 
@@ -132,27 +199,55 @@ def get_text_components(original_image, components):
     return text_components
 
 def highlight_components(PBM_image, components):
+    """ Highlight the components by outlining the components on the original
+    image
+
+    Args:
+    PBM_image(np.ndarray): original PBM image
+    components(list): components to be outlined
+
+    Returns:
+    np.ndarray: image with highlighted components
+
+    """
     image = PBM_image.copy()
     for component in components:
         x_min, y_min, w, h, area = component
         for i in range(h):
             y = y_min + i
-            image[y][x_min] = 0
-            image[y][x_min + w - 1] = 0
+            image[y, x_min] = 0
+            image[y, x_min + w - 1] = 0
         for i in range(w):
             x = x_min + i
-            image[y_min][x] = 0
-            image[y_min + h - 1][x] = 0
+            image[y_min, x] = 0
+            image[y_min + h - 1, x] = 0
     return image
 
 def get_words_components_from_text_components(original_image, text_components):
+    """ Get the word components in a original image for each given line
+    component
+
+    Args:
+    original_image(np.ndarray): original image
+    text_components(list): list of line components
+
+    Returns:
+    list: word components
+    list: number of words per line
+    """
     preprocessed_image = find_words_preprocessing(original_image)
     words_components = []
+    words_per_line = []
     for component in text_components:
         x_min, y_min, w, h, area = component
-        words_components.extend(get_components_coordenates(original_image
-            [y_min:y_min + h - 1][x_min:x_min + w - 1])[1:])
-    return words_components
+        component_slice = preprocessed_image[y_min:y_min + h - 1, x_min:x_min + w - 1].copy()
+        w_components = get_components_coordenates(component_slice)[1:]
+        for c in w_components:
+            c[0] = c[0] + x_min
+            c[1] = c[1] + y_min
+        words_components.extend(w_components)
+        words_per_line.append(len(w_components))
+    return words_components, words_per_line
 
 def main():
     filename = "bitmap.pbm"
@@ -160,9 +255,13 @@ def main():
     image = preprocessing(original_image)
     components = get_components_coordenates(image)
     text_components = get_text_components(original_image, components)
-    words_components = get_words_components_from_text_components(original_image, text_components)
+    words_components, words_per_line = get_words_components_from_text_components(
+    original_image, text_components)
     highlighted_lines = highlight_components(original_image, text_components)
-    highlighted_words = highlight_components(highlighted_lines, words_components)
+    highlighted_words = highlight_components(original_image, words_components)
+    print("number of lines: {}, total number of words: {}".format(len(
+    text_components), len(words_components)))
+    print("number of words per line: {}".format(words_per_line))
     save_image(highlighted_lines, filename, "highlighted_lines")
     save_image(highlighted_words, filename, "highlighted_words")
 
